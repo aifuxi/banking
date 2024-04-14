b6d643bcce4681a7b27507521269b7a8bc9560be
fuxiaochen <aifuxi.js@gmail.com>
2024-04-14 22:38:53 +0800

    feat: add customers endpoint

--- main.go
+++ main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type Customer struct {
+	Name    string
+	City    string
+	ZipCode string
+}
+
 func main() {
 
 	http.HandleFunc("/greet", greet)
+	http.HandleFunc("/customers", getAllCustomers)
 
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World ~")
+}
+
+func getAllCustomers(w http.ResponseWriter, r *http.Request) {
+	customers := []Customer{
+		{"Tom", "bj", "001"},
+		{"Jerry", "sh", "002"},
+		{"GG Bound", "hz", "0010"},
+	}
+	// add content type header
+	w.Header().Add("Content-Type", "application/json")
+
+	// customers -> response
+	json.NewEncoder(w).Encode(customers)
 }
