21975879f0e9d1dc4bb3851dac400ade6f6be7a1
fuxiaochen <aifuxi.js@gmail.com>
2024-04-16 23:51:42 +0800

    feat: use sqlx to optimize sql query

--- domain/customer_repository_db.go
+++ domain/customer_repository_db.go
@@ -6,47 +6,35 @@ import (
 	"github.com/aifuxi/banking/errs"
 	"github.com/aifuxi/banking/logger"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"go.uber.org/zap"
 	"time"
 )
 
 type CustomerRepositoryDb struct {
-	client *sql.DB
+	client *sqlx.DB
 }
 
 func (d CustomerRepositoryDb) FindAll() ([]Customer, error) {
+	customers := make([]Customer, 0)
 
 	findAllSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers"
 
-	rows, err := d.client.Query(findAllSql)
-	if err != nil {
+	if err := d.client.Select(&customers, findAllSql); err != nil {
 		logger.Error("error while querying customers ", zap.Field{Key: "error", String: err.Error()})
 		return nil, err
-	}
-	customers := make([]Customer, 0)
-
-	for rows.Next() {
-		var c Customer
-
-		err := rows.Scan(&c.Id, &c.Name, &c.City, &c.Zipcode, &c.DateOfBirth, &c.Status)
-		if err != nil {
-			logger.Info("error while scanning customers ", zap.Field{Key: "error", String: err.Error()})
-			return nil, err
-		}
-		customers = append(customers, c)
 	}
 
 	return customers, nil
 }
 
 func (d CustomerRepositoryDb) ById(id string) (*Customer, *errs.AppErr) {
+	var c Customer
 
 	customerSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers where customer_id = ?"
 
-	row := d.client.QueryRow(customerSql, id)
-	var c Customer
-	err := row.Scan(&c.Id, &c.Name, &c.City, &c.Zipcode, &c.DateOfBirth, &c.Status)
-	if err != nil {
+	if err := d.client.Get(&c, customerSql, id); err != nil {
+		logger.Debug("get customer debug" + err.Error())
 		// check query row error
 		if errors.Is(err, sql.ErrNoRows) {
 			logger.Error("sql.ErrNoRows" + sql.ErrNoRows.Error())
@@ -61,7 +49,7 @@ func (d CustomerRepositoryDb) ById(id string) (*Customer, *errs.AppErr) {
 }
 
 func NewCustomerRepositoryDb() CustomerRepositoryDb {
-	client, err := sql.Open("mysql", "root:123456@/banking")
+	client, err := sqlx.Open("mysql", "root:123456@/banking")
 	if err != nil {
 		logger.Error("sql open error: " + err.Error())
 	}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require github.com/gorilla/mux v1.8.1
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
+	github.com/jmoiron/sqlx v1.3.5 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 )
